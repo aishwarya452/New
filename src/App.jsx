910928c569c090509b0437eddc2230f5ca26356b
Aishwarya <aishwaryachavan648@gmail.com>
2025-06-26 12:42:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+
+
+import './App.css';
+
+import CoursesPage from "./Pages/CoursesPage";
+import AboutPage from "./Pages/AboutPage";
+import HomePage from "./Pages/HomePage";
+import ContactPage from "./Pages/ContactPage";
+import ChatbotComponent from "./components/Chatbot/ChatbotComponent";
+import Header from "./components/Header/Header";
+import AdmissionPage from "./Pages/AdmissionPage";
+
+
+
+
+const App =() =>{
+  return(
+    <div>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/about" element={<AboutPage/>} />
+           <Route path="/courses" element={<CoursesPage/>} />
+          <Route path="/contact" element={<ContactPage/>} />
+          <Route path="/admission" element={<AdmissionPage/>} />
+          
+          
+        </Routes>
+        <ChatbotComponent/>
+      </Router>
+      
+      
+      
+
+    </div>
+  )
+}
+
+export default App;
