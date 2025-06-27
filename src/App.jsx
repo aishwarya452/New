fbf7ed5b5670180d69c13c9b01348eec7ee85fc4
Aishwarya <aishwaryachavan648@gmail.com>
2025-06-27 09:06:06 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 
 
-import HomePage from "./Pages/HomePage";
 import ContactPage from "./Pages/ContactPage";
 import ChatbotComponent from "./components/Chatbot/ChatbotComponent";
 import Header from "./components/Header/Header";
 // import AdmissionPage from "./Pages/AdmissionPage";
 import CoursesPage from "./Pages/CoursesPage";
 import AboutPage from "./Pages/AboutPage";
+import HomePage from "./Pages/HomePage";
  
 
 
